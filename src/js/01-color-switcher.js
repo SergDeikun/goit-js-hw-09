719cddf5cdcc6b94680cbb8bb232d1cd58fb6c8b
SergDeikun <dsv2807@gmail.com>
2022-05-13 18:47:39 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+};
 
+let intervalId = null;
+
+refs.startBtn.addEventListener('click', handleStartClick);
+refs.stopBtn.addEventListener('click', handleStopClick);
+
+function handleStartClick() {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  refs.startBtn.setAttribute('disabled', 'disabled');
+  refs.stopBtn.removeAttribute('disabled');
+}
+
+function handleStopClick() {
+  clearInterval(intervalId);
+
+  refs.startBtn.removeAttribute('disabled');
+  refs.stopBtn.setAttribute('disabled', 'disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
